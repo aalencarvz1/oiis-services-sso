9f46bf13b7b9b1c7780d8f7cad4f1868126ecd39
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2025-09-25 08:43:14 -0300

    adjusted logback

---
+++ src/main/java/com/oiis/services/sso/config/TomcatConfig.java
@@ -0,0 +1,24 @@
+package com.oiis.services.sso.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Value("${INTERNAL_PORT}")
+    private Integer INTERNAL_PORT;
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return server -> {
+            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            connector.setPort(INTERNAL_PORT);
+            server.addAdditionalTomcatConnectors(connector);
+        };
+    }
+}
