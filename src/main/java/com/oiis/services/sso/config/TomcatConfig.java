be3ee09db4dbcbfd032f0d24048c113988f05dea
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2025-10-16 10:04:36 -0300

    fixed .env dependency on tomcat config

--- src/main/java/com/oiis/services/sso/config/TomcatConfig.java
+++ src/main/java/com/oiis/services/sso/config/TomcatConfig.java
@@ -8,19 +8,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Configure server to allow run with external port configured on application.yml and different internal port, wich no requires https
+ * Configure server to allow run with external port configured on application.yml or .env file and different local port, wich no requires https
  */
 @Configuration
 public class TomcatConfig {
 
-    @Value("${INTERNAL_PORT}")
-    private Integer INTERNAL_PORT;
+    @Value("${server.local-port}")
+    private Integer LOCAL_PORT;
 
     @Bean
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
         return server -> {
             Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-            connector.setPort(INTERNAL_PORT);
+            connector.setPort(LOCAL_PORT);
             server.addAdditionalTomcatConnectors(connector);
         };
     }
